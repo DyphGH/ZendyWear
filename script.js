f80ced3a332c59ee8a9faca1eadc2d3d26cc5e1d
DyphGH <125663693+DyphGH@users.noreply.github.com>
2025-04-08 19:12:02 +0100

    script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-const cursorLight = document.querySelector('.cursor-light');
-
-document.addEventListener('mousemove', (e) => {
-  cursorLight.style.top = `${e.clientY}px`;
-  cursorLight.style.left = `${e.clientX}px`;
+document.addEventListener("mousemove", (e) => {
+  const cursorLight = document.querySelector(".cursor-light");
+  if (cursorLight) {
+    cursorLight.style.top = `${e.clientY}px`;
+    cursorLight.style.left = `${e.clientX}px`;
+  }
 });
