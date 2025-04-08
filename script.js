f890738a71e541f624898ffe6845321730546e99
DyphGH <125663693+DyphGH@users.noreply.github.com>
2025-04-08 20:07:40 +0100

    script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener("mousemove", (e) => {
   const cursorLight = document.querySelector(".cursor-light");
   if (cursorLight) {
-    cursorLight.style.top = `${e.pageY}px`;
-    cursorLight.style.left = `${e.pageX}px`;
+    cursorLight.style.top = `${e.pageY + scrollY}px`;
+    cursorLight.style.left = `${e.pageX + scrollX}px`;
   }
 });
