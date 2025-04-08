3ea615c3982df097c53d57f595fbac10a320fb64
DyphGH <125663693+DyphGH@users.noreply.github.com>
2025-04-08 22:35:18 +0100

    script.js

--- script.js
+++ script.js
@@ -5,3 +5,23 @@ document.addEventListener("mousemove", (e) => {
     cursorLight.style.left = `${e.pageX}px`;
   }
 });
+const tooltip = document.getElementById("global-tooltip");
+
+document.querySelector(".search-icon-wrapper").addEventListener("mouseenter", () => {
+  tooltip.innerText = "Search";
+  tooltip.style.opacity = "1";
+});
+document.querySelector(".user-icon-wrapper").addEventListener("mouseenter", () => {
+  tooltip.innerText = "Login / Register";
+  tooltip.style.opacity = "1";
+});
+document.querySelector(".cart-icon-wrapper").addEventListener("mouseenter", () => {
+  tooltip.innerText = "Cart";
+  tooltip.style.opacity = "1";
+});
+
+document.querySelectorAll(".search-icon-wrapper, .user-icon-wrapper, .cart-icon-wrapper").forEach(el => {
+  el.addEventListener("mouseleave", () => {
+    tooltip.style.opacity = "0";
+  });
+});
