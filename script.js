f28b017ba492ecd88b001fcc45171a0f95bc5b99
DyphGH <125663693+DyphGH@users.noreply.github.com>
2025-04-09 20:18:56 +0100

    script.js

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-// Adiciona a classe .scrolled quando der scroll
-window.addEventListener("scroll", () => {
-  const body = document.body;
-  if (window.scrollY > 10) {
-    body.classList.add("scrolled");
-  } else {
-    body.classList.remove("scrolled");
-  }
-});
+
