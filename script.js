94ae61fb925582f1c07e5786769f821b0bcc2959
DyphGH <125663693+DyphGH@users.noreply.github.com>
2025-04-09 22:02:49 +0100

    script.js

--- script.js
+++ script.js
@@ -7,3 +7,12 @@ window.addEventListener('DOMContentLoaded', () => {
 window.addEventListener('DOMContentLoaded', () => {
   document.body.classList.add('show');
 });
+// Fade in da página inteira
+window.addEventListener('DOMContentLoaded', () => {
+  document.body.classList.add('show');
+
+  // Ativa fade-in em elementos específicos
+  document.querySelectorAll('.fade-in').forEach(el => {
+    el.classList.add('show');
+  });
+});
