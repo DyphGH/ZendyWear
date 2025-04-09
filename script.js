109e41ced17e621f5d77a16f2b3520fed9aad273
DyphGH <125663693+DyphGH@users.noreply.github.com>
2025-04-09 21:38:08 +0100

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-
+// Faz fade-in suave nos elementos com a classe .fade-in
+window.addEventListener('DOMContentLoaded', () => {
+  document.querySelectorAll('.fade-in').forEach(el => {
+    el.classList.add('show');
+  });
+});
